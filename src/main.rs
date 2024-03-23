1c9dbde5703ba3078ede626b9236e6cf8a568b5e
Mohammed Mahfoud <mhd.mahfoud@hotmail.com>
2024-03-23 15:44:36 +0300

    1- root now takes a function to calculate the next term of the Newton method 2- move test cases to test mod

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,11 @@ fn power(x: f64, n: u32) -> f64 {
 
 /// Approximation order
 const EPSILON: f64 = 1e-20;
+
 /// Maximum number of iterations
 const MAX_ITERATIONS: u32 = 1_000_000;
 
-fn next(x: f64, n: u32, p: f64) -> f64 {
+fn basic_next(x: f64, n: u32, p: f64) -> f64 {
     // Un+1 = Un - (Un^n - x)/(n.Un^(n-1))
     p - ((power(p, n) - x) / ((n as f64) * power(p, n - 1)))
 }
@@ -44,7 +45,10 @@ fn next(x: f64, n: u32, p: f64) -> f64 {
 ///
 /// assert!((answer - 2.0).abs() >= crate::EPSILON);
 /// ```
-fn root(x: f64, n: u32) -> f64 {
+fn root<F>(x: f64, n: u32, next: F) -> f64
+where
+    F: Fn((f64, u32, f64)) -> f64,
+{
     // return 0 for values of x too small
     if x.abs() <= EPSILON {
         return 0.0;
@@ -54,59 +58,83 @@ fn root(x: f64, n: u32) -> f64 {
     let mut p = if x >= 0.0 { 1.0 } else { -1.0 };
 
     // calculate next value
-    let mut u = next(x, n, p);
+    let mut u = next((x, n, p));
 
     let mut i: u32 = 1;
     while (((u - p) / p).abs() >= EPSILON) && (i <= MAX_ITERATIONS) {
         p = u;
-        u = next(x, n, p);
+        u = next((x, n, p));
         i += 1;
     }
     u
 }
 
-/// Represents a test case to be checked. it contains a number (expected_root) and a vector of its powers starting from its square.
-struct TestCase {
-    values: Vec<f64>,
-    expected_root: f64,
+fn main() {
+    let sqrt_4 = root(4.0, 2, |(a, b, c)| basic_next(a, b, c));
+    println!("4^(1/2) = {}", sqrt_4);
+
+    println!(
+        "(81.0)^(1/4) = {}",
+        root(81.0, 4, |(a, b, c)| (((b as f64) - 1.0) * power(c, b) + a)
+            / ((b as f64) * power(c, b - 1)))
+    );
 }
 
-fn main() {
-    // initialize test case with numbers between 0 and 1000 with their powers from 2 to 10.
-    let mut test_cases: Vec<TestCase> = (0..=1000)
-        .map(|x| TestCase {
-            expected_root: x as f64,
-            values: (2..=10).map(|y| power(x as f64, y)).collect(),
-        })
-        .collect();
+#[cfg(test)]
+mod tests {
+    use crate::*;
 
-    // add a test case for square root of 8
-    test_cases.push(TestCase {
-        expected_root: 2.82842712474619,
-        values: vec![8.0],
-    });
+    /// Represents a test case to be checked. it contains a number (expected_root) and a vector of its powers starting from its square.
+    struct TestCase {
+        values: Vec<f64>,
+        expected_root: f64,
+    }
 
-    // perform calculations on each test case
-    for test in test_cases {
-        let roots: Vec<(f64, u32, f64, f64, bool)> = test
-            .values
-            .iter()
-            .enumerate()
-            .map(|(i, v)| {
-                let r = root(*v, (i as u32) + 2);
-                (
-                    test.expected_root,
-                    (i as u32) + 2,
-                    *v,
-                    r,
-                    (r - test.expected_root).abs() < EPSILON,
-                )
+    #[test]
+    fn it_works() {
+        // initialize test case with numbers between 0 and 1000 with their powers from 2 to 10.
+        let mut test_cases: Vec<TestCase> = (0..=10)
+            .map(|x| TestCase {
+                expected_root: x as f64,
+                values: (2..=10).map(|y| power(x as f64, y)).collect(),
             })
             .collect();
 
-        // print test results for this test case
-        for (x, n, v, r, b) in roots {
-            println!("{}: {}v{} = {} => {}", x, n, v, r, b);
+        // add a test case for square root of 8
+        test_cases.push(TestCase {
+            expected_root: 2.82842712474619,
+            values: vec![8.0],
+        });
+
+        // add a test case for square root of 8
+        test_cases.push(TestCase {
+            expected_root: 4.9999999999999999, // Should be 5 but 4.9999999999999999 is close enough (<= EPSILON 1e-20)
+            values: vec![25.0],
+        });
+
+        // perform calculations on each test case
+        for test in test_cases {
+            let roots: Vec<(f64, u32, f64, f64, bool)> = test
+                .values
+                .iter()
+                .enumerate()
+                .map(|(i, v)| {
+                    let r = root(*v, (i as u32) + 2, |(a, b, c)| basic_next(a, b, c));
+                    (
+                        test.expected_root,
+                        (i as u32) + 2,
+                        *v,
+                        r,
+                        (r - test.expected_root).abs() < EPSILON,
+                    )
+                })
+                .collect();
+
+            // print test results for this test case
+            for (x, n, v, r, b) in roots {
+                println!("{}th root of {} expected={} found={}! => {}", n, v, x, r, b);
+                assert!(b, "{}th root of {} expected={} found={}!", n, v, x, r);
+            }
         }
     }
 }
